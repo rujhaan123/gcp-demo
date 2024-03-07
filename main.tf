756a378c979ebcc1d16fb20ae4bf78eacec7bf33
Rujhaan Bhatia <33410430+rujhaan123@users.noreply.github.com>
2024-03-07 12:47:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,19 +41,3 @@ resource "google_compute_instance" "default" {
     }
   }
 }
-
-resource "google_storage_bucket" "default" {
-  name          = "rb_testbuck123"
-  force_destroy = false
-  location      = "US"
-  storage_class = "STANDARD"
-  versioning {
-    enabled = true
-  }
-  /*encryption {
-    default_kms_key_name = google_kms_crypto_key.terraform_state_bucket.id
-  }
-  depends_on = [
-    google_project_iam_member.default
-  ]*/
-}
